703abf762ca4e52afa6e0e38de7c3da45cddd54f
jddelia <jddelia123@gmail.com>
2018-12-28 16:28:18 -0500

    Inital commit, prepped for heroku deploy

---
+++ build/precache-manifest.a402ea321305532cbc796a297b08273b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "160befe85c4a963065e9",
+    "url": "/static/js/main.160befe8.chunk.js"
+  },
+  {
+    "revision": "c68593c6f53c8ee4d324",
+    "url": "/static/js/1.c68593c6.chunk.js"
+  },
+  {
+    "revision": "160befe85c4a963065e9",
+    "url": "/static/css/main.fea039e7.chunk.css"
+  },
+  {
+    "revision": "bee0719a890d567a336f7cf3571aba0d",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SampleLibrary from './tonejs-instruments-master/Tonejs-Instruments';
+import Header from './components/Header';
+import Note from './components/Note';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Control from './components/Control';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedInstr: "guitar-acoustic",
+      instrument: null,
+      notes: []
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleInstrumentChange = this.handleInstrumentChange.bind(this);
+  }
+
+  componentDidMount() {
+    // Create instrument
+    this.setState({
+      instrument: SampleLibrary.load({
+        instruments: this.state.selectedInstr,
+        minify: true
+      }),
+      notes: SampleLibrary[this.state.selectedInstr] ? SampleLibrary[this.state.selectedInstr]: []
+    });
+  }
+
+  handleClick(note) {
+    // Play note from instrument
+    this.state.instrument.toMaster();
+
+    this.state.instrument.triggerAttack(note);
+  }
+
+  handleInstrumentChange(instrument, index) {
+    this.setState((prevState, props) => ({
+      selectedInstr: instrument
+    }), () => {
+      this.setState({
+        instrument: SampleLibrary.load({
+          instruments: this.state.selectedInstr,
+          minify: true
+        }),
+        notes: SampleLibrary[this.state.selectedInstr]
+      });
+    });
+  }
+
   render() {
+    let notesArr = [];
+    for (let note in this.state.notes) {
+      notesArr.push(note)
+    }
+
+    const notes = notesArr.map((note, index) => {
+      return (
+        <Note key={index} note={note} onClick={this.handleClick} />
+      );
+    })
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="">
+        <Navbar />
+        <Header />
+        <div className="container">
+          <About />
+          <Control onChange={this.handleInstrumentChange} />
+          <div className="chords">
+            {notes}
+          </div>
+        </div>
       </div>
     );
   }
